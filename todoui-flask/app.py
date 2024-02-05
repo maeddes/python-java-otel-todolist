076c87439e96561ca61e0f41f112fd36f8cd521b
maeddes <maeddes@gmail.com>
2024-02-05 21:49:19 +0000

    working version

---
+++ todoui-flask/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+
+import requests
+import os
+
+app = Flask(__name__)
+
+# Set a default external API URL
+app.config['BACKEND_URL'] = 'http://localhost:8080/todos/'
+
+# Override the default URL if an environment variable is set
+app.config['BACKEND_URL'] = os.getenv('BACKEND_URL', app.config['BACKEND_URL'])
+
+# Sample data: list of ToDo items (plain strings)
+todos = ["Learn Flask", "Build a CRUD app", "Deploy the app"]
+
+@app.route('/')
+def index():
+
+    response = requests.get(app.config['BACKEND_URL'])
+    
+    if response.status_code == 200:
+        # Print out the response content
+        print(response.text)
+        todos = response.json()
+        
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add():
+
+    if request.method == 'POST':
+        new_todo = request.form['title']
+        response = requests.post(app.config['BACKEND_URL']+new_todo)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<string:todo>')
+def delete(todo):
+
+    response = requests.delete(app.config['BACKEND_URL']+todo)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
